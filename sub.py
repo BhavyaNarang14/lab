11cee6b33ebbfadcc5298f85026a67d66ed21d08
bhavyanarang14 <narang.bhavya@gmail.com>
2020-08-12 13:03:45 +0530

    Bhavya's Sub

--- sub.py
+++ sub.py
@@ -1,10 +1,15 @@
-x = int(input("Enter your first no."))
-y = int(input("Enter your second no."))
+x = int(input("Enter your first no.: "))
+y = int(input("Enter your second no.: "))
+def sub(x,y):
+    
+    if x>y:
+        z = x-y
+        return z
+    elif y>x:
+        z = y-x
+        return z
+    else:
+        z = 0
+        return z
 
-if x>y:
-    print(x-y)
-elif y>x:
-    print(y-x)
-else:
-    print("0")
-        
+print("Difference of x and y is: ", sub(x,y))
