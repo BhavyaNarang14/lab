659f5a298590dd08489b4a379f4f22265591269a
Anushree <61469997+anu180@users.noreply.github.com>
2020-08-12 21:16:37 +0530

    anushree's add

--- add.py
+++ add.py
@@ -1,8 +1,8 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
 def add(x,y):
    sum = x + y
    return sum
 
-num1 = 2
-num2 = 6
 
-print("The sum is", add(num1, num2))
+print("The sum is", add(x, y))
