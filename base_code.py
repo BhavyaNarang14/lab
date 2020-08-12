2e72cfedb302d4fc3218560a2e94c2158e118f0e
bhavyanarang14 <narang.bhavya@gmail.com>
2020-08-12 15:12:00 +0530

    MERGED CODE

--- base_code.py
+++ base_code.py
@@ -1,25 +1,44 @@
 print("""
-For add press '+'
-For sub press '-'
-For mul press 'x'
-For div press '/'
+For add press '1'
+For sub press '2'
+For mul press '3'
+For div press '4'
 """)
 
+i = int(input("enter your choice:"))
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+def add(x,y):
+    sum = x + y
+    return sum
+def sub(x,y):
+    
+    if x>y:
+        z = x-y
+        return z
+    elif y>x:
+        z = y-x
+        return z
+    else:
+        z = 0
+        return z
+def mul(x,y):
+    z = x*y
+    return z
+def divide(x, y):
+    if (y == 0):
+        print ('Not possible')
+    return x / y
 
-i = input("enter your choice:")
-switch(i) { 
-        case 1: 
-            return "zero";
-        break;
-        case 2: 
-            return "one";
-        break;
-        case 3: 
-            return "two";
-        break;
-        case 4: 
-            return "nothing";
-        break;
-        default:
-            print("no case")
-    }; 
+if i==1:
+    print(add(x,y))
+elif i==2:
+    print(sub(x,y))
+elif i==3:
+    print(mul(x,y))
+elif i==4:
+    div = divide(x,y)
+    print(div)
+else:
+    print("numbers not valid")
+
